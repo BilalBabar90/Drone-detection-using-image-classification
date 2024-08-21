a2d9c4cfc7793d35368c13309d03ae3d414cf5ad
BilalBabar90 <babarbilal032@gmail.com>
2024-08-21 14:45:30 +0500

    Fine tuned yolov8 model weights, also tried some other pretrained models

---
+++ model_load.py
@@ -0,0 +1,11 @@
+from keras.models import load_model
+
+# Path to your downloaded .h5 file
+model_path = r"C:\Users\Dell\Downloads\model_checkpoint_Densenet.h5"
+
+
+# Load the model
+model = load_model(model_path)
+
+# Now you can use the loaded model for prediction or further analysis
+print("bilal")
